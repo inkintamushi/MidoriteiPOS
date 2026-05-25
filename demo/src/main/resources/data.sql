3caf229ba400ee3e04e225916bd2d038850e3cc2
inkintamushi <244089@ocsjoho.onmicrosoft.com>
2026-05-25 10:08:12 +0900

    変更

--- demo/src/main/resources/data.sql
+++ demo/src/main/resources/data.sql
@@ -2,10 +2,10 @@ MERGE INTO products (id, name, category, price, image_path, sold_out) KEY(id)
 VALUES (1, 'つくね', 'yaki', 180, '/images/product3.jpg', FALSE);
 
 MERGE INTO products (id, name, category, price, image_path, sold_out) KEY(id)
-VALUES (2, '酒井高徳', 'alcohol', 550, '/images/product2.jpg', FALSE);
+VALUES (2, '日本酒', 'alcohol', 550, '/images/product2.jpg', FALSE);
 
 MERGE INTO products (id, name, category, price, image_path, sold_out) KEY(id)
-VALUES (3, 'ドリンクウォーター', 'alcohol', 320, '/images/product5.jpg', FALSE);
+VALUES (3, '水', 'alcohol', 320, '/images/product5.jpg', FALSE);
 
 MERGE INTO products (id, name, category, price, image_path, sold_out) KEY(id)
 VALUES (4, 'いなり', 'yaki', 450, '/images/product7.jpg', FALSE);
@@ -17,6 +17,6 @@ MERGE INTO products (id, name, category, price, image_path, sold_out) KEY(id)
 VALUES (6, '飲み放題プラン', 'nomi', 1980, '/images/product6.jpg', FALSE);
 
 MERGE INTO products (id, name, category, price, image_path, sold_out) KEY(id)
-VALUES (7, 'くにくにの鮓冗坩', 'yaki', 160, '/images/product4.jpg', FALSE);
+VALUES (7, 'くにくにの鶏皮', 'yaki', 160, '/images/product4.jpg', FALSE);
 
 ALTER TABLE products ALTER COLUMN id RESTART WITH 8;
